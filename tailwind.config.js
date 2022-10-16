e9b214b727427673e28cd8d07fd788631644d8de
kh-almas <services.almas@gmail.com>
2022-10-17 04:54:28 +0600

    user_onboarding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/*.{html,js}"],
+  content: [
+      "./public/*.{html,js}",
+      "./public/**/*.{html,js}",
+  ],
   theme: {
     extend: {},
   },
